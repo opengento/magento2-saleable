0283003e1e308ab050d4c9c76ab4e2a03c267eae
Thomas Klein <thomasklein876@gmail.com>
2025-02-10 11:53:11 +0100

    Apply default values

--- Setup/Patch/Data/ProductAttributes.php
+++ Setup/Patch/Data/ProductAttributes.php
@@ -8,20 +8,35 @@ declare(strict_types=1);
 namespace Opengento\Saleable\Setup\Patch\Data;
 
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product as ResourceProduct;
 use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Sql\Expression;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\Store;
 use Zend_Validate_Exception;
 
 final class ProductAttributes implements DataPatchInterface
 {
     public function __construct(
         private ModuleDataSetupInterface $moduleDataSetup,
-        private EavSetupFactory $eavSetupFactory
+        private EavSetupFactory $eavSetupFactory,
+        private ResourceProduct $productResource
     ) {}
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
 
     /**
      * @throws Zend_Validate_Exception
@@ -34,6 +49,14 @@ final class ProductAttributes implements DataPatchInterface
         /** @var EavSetup $eavSetup */
         $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
 
+        $this->addAttributes($eavSetup);
+        $this->addDefaultValues($eavSetup);
+
+        $this->moduleDataSetup->endSetup();
+    }
+
+    private function addAttributes(EavSetup $eavSetup): void
+    {
         $eavSetup->addAttribute(Product::ENTITY, 'is_purchasable', [
             'type' => 'int',
             'backend' => '',
@@ -76,17 +99,42 @@ final class ProductAttributes implements DataPatchInterface
             'unique' => false,
             'apply_to' => '',
         ]);
-
-        $this->moduleDataSetup->endSetup();
     }
 
-    public function getAliases(): array
+    private function addDefaultValues(EavSetup $eavSetup): void
     {
-        return [];
+        foreach (['is_purchasable', 'can_show_price'] as $attributeCode) {
+            $this->addDefaultValue($eavSetup, $eavSetup->getAttribute(Product::ENTITY, $attributeCode));
+        }
     }
-
-    public static function getDependencies(): array
+    
+    private function addDefaultValue(EavSetup $eavSetup, array $attribute): void
     {
-        return [];
+        $connection = $this->productResource->getConnection();
+        $select = $connection
+            ->select()
+            ->from(
+                $this->productResource->getEntityTable(),
+                [
+                    'attribute_id' => new Expression($attribute['attribute_id']),
+                    'store_id' => new Expression(Store::DEFAULT_STORE_ID),
+                    'value' => new Expression(1),
+                    $this->productResource->getLinkField(),
+                ]
+            );
+
+        $insert = $connection->insertFromSelect(
+            $select,
+            $connection->getTableName('catalog_product_entity_int'),
+            [
+                'attribute_id',
+                'store_id',
+                'value',
+                $this->productResource->getLinkField()
+            ],
+            AdapterInterface::INSERT_IGNORE
+        );
+
+        $connection->query($insert);
     }
 }
