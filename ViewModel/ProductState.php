933c86bf8dc0cb84d71863772325a5008b1187b9
Thomas Klein <thomasklein876@gmail.com>
2025-04-03 23:32:17 +0200

    Create ProductState.php

---
+++ ViewModel/ProductState.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © OpenGento, All rights reserved.
+ * See LICENSE bundled with this library for license details.
+ */
+declare(strict_types=1);
+
+namespace Sev\Catalog\ViewModel;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Opengento\Saleable\Api\IsSaleableInterface;
+use Opengento\Saleable\Model\CurrentCustomerGroupId;
+
+final class ProductState implements ArgumentInterface
+{
+    public function __construct(
+        private IsSaleableInterface $isSaleable,
+        private CurrentCustomerGroupId $currentCustomerGroupId
+    ) {}
+
+    /**
+     * Check wether or not the product could be purchased.
+     * Does not check if the product is actually salable (meaning it is enabled and in stock/backorder).
+     */
+    public function isPurchasable(Product $product): bool
+    {
+        return $product->getData('is_purchasable') && $this->isSaleable->isSaleable($this->currentCustomerGroupId->get());
+    }
+}
